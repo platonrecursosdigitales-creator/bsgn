0d13f92db7780a11b855a2bed984a5332e3ed9fd
platonrecursosdigitales-creator <brandondamid.bd@gmail.com>
2026-07-21 13:54:22 -0600

    Update title and description in Metrics component

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -8,12 +8,12 @@ function Metrics() {
   return (
     <section className="skyscraper-section" id="nosotros">
       <div className="container skyscraper-content">
-        <h2 className="skyscraper-title">EXPERIENCIAS DE<br/>ALTO NIVEL</h2>
+        <h2 className="skyscraper-title">Strategic Intelligence &<br/>High-impact experiences.</h2>
         <div className="skyscraper-divider"></div>
         <p className="skyscraper-description">
-          Como consultoría de comunicación estratégica y empresarial, nuestra prioridad<br/>
-          es centrarnos en la importancia de los detalles para reformular el impacto de tu<br/>
-          marca y desarrollar experiencias de alto nivel.
+          Convertimos conocimiento, percepción y estrategia en soluciones que impulsan el crecimiento sostenible de líderes y organizaciones, nos enfocamos en cada detalle para transformar tus resultados implementando estrategias y desarrollando experiencias de alto impacto.
+          <br/><br/>
+          Hemos creado e impulsado más de 20 líderes de mercado en distintas industrias.
         </p>
         <div className="skyscraper-image-container">
           <img src={heroImg} alt="Edificio corporativo" className="skyscraper-circular-image" />
